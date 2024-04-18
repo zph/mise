0b58e8aaf4896d7e30b78243db1b110f588c8e24
Zander Hill <zander@xargs.io>
2024-04-17 21:13:06 -0500

    Add plain binary fetching via ubi

--- src/forge/ubi.rs
+++ src/forge/ubi.rs
@@ -7,7 +7,6 @@ use crate::config::{Config, Settings};
 
 use crate::forge::{Forge, ForgeType};
 use crate::http::HTTP_FETCH;
-use itertools::Itertools;
 use url::Url;
 use crate::install_context::InstallContext;
 use crate::toolset::ToolVersion;
@@ -20,6 +19,8 @@ pub struct UBIForge {
     latest_version_cache: CacheManager<Option<String>>,
 }
 
+// Uses ubi for installations https://github.com/houseabsolute/ubi
+// it can be installed via mise install cargo:houseabsolute/ubi
 impl Forge for UBIForge {
     fn get_type(&self) -> ForgeType {
         ForgeType::Ubi
@@ -35,17 +36,17 @@ impl Forge for UBIForge {
 
     // TODO: v0.0.3 is stripped of 'v' such that it reports incorrectly in tool :-/
     fn list_remote_versions(&self) -> eyre::Result<Vec<String>> {
-        self.remote_version_cache
-            .get_or_try_init(|| {
-                let raw = HTTP_FETCH.get_text(get_binary_url(self.name())?)?;
-                let releases: Value = serde_json::from_str(&raw)?;
-                let mut versions = vec![];
-                for v in releases.as_array().unwrap() {
-                    versions.push(v["tag_name"].as_str().unwrap().to_string());
-                }
-                Ok(versions)
-            })
-            .cloned()
+      self.remote_version_cache
+          .get_or_try_init(|| {
+              let raw = HTTP_FETCH.get_text(get_binary_url(self.name())?)?;
+              let releases: Value = serde_json::from_str(&raw)?;
+              let mut versions = vec![];
+              for v in releases.as_array().unwrap() {
+                  versions.push(v["tag_name"].as_str().unwrap().to_string());
+              }
+              Ok(versions)
+          })
+          .cloned()
     }
 
     fn latest_stable_version(&self) -> eyre::Result<Option<String>> {
@@ -63,18 +64,31 @@ impl Forge for UBIForge {
         // Workaround because of not knowing how to pull out the value correctly without quoting
         let matching_version = self.list_remote_versions()?.into_iter().find(|v| v.contains(&ctx.tv.version)).unwrap().replace("\"", "");
         let path_without_quotes = ctx.tv.install_path().to_str().unwrap().replace("\"", "");
+        let path_with_bin = path_without_quotes + "/bin/";
 
-        CmdLineRunner::new("ubi")
-            .arg("--project")
-            .arg(&format!("{}", self.name()))
-            .arg("--tag")
-            .arg(matching_version)
-            .arg("--in")
-            .arg(path_without_quotes + "/bin")
-            .with_pr(ctx.pr.as_ref())
-            .envs(config.env()?)
-            .prepend_path(ctx.ts.list_paths())?
-            .execute()?;
+        // TODO(zph): Extract commonality to apply to each variant
+        if self.name().starts_with("http") {
+          CmdLineRunner::new("ubi")
+              .arg("--url")
+              .arg(&format!("{}", self.name()))
+              .arg("--in")
+              .arg(path_with_bin)
+              .with_pr(ctx.pr.as_ref())
+              .envs(config.env()?)
+              .prepend_path(ctx.ts.list_paths())?
+              .execute()?;
+        } else {
+          CmdLineRunner::new("ubi")
+              .arg("--project")
+              .arg(&format!("{}", self.name()))
+              .arg("--tag").arg(matching_version)
+              .arg("--in")
+              .arg(path_with_bin)
+              .with_pr(ctx.pr.as_ref())
+              .envs(config.env()?)
+              .prepend_path(ctx.ts.list_paths())?
+              .execute()?;
+        }
 
         Ok(())
     }
@@ -92,19 +106,12 @@ impl UBIForge {
     }
 }
 
-// TODO: allow to specify a binary based on URL only via UBI flag --url
-//   -u, --url <url>            The url of the file to download. This can be provided instead of a
-//   project or tag. This will not use the GitHub API, so you will never hit
-//   the GitHub API limits. This means you do not need to set a GITHUB_TOKEN
-//   env var except for private repos.
 fn get_binary_url(n: &str) -> eyre::Result<Url> {
     let n = n.to_lowercase();
-    let url = match n.split("/").try_len()? {
-        // Support GH shorthand
-        2 => format!("https://api.github.com/repos/{n}/releases"),
-        // Support GH longhand
-        // Support arbitrary URL link
-        _ => n,
-    };
-    Ok(url.parse()?)
+    if n.starts_with("https://") || n.starts_with("http://") {
+      return Ok(n.parse()?);
+    } else {
+      let url = format!("https://api.github.com/repos/{n}/releases");
+      Ok(url.parse()?)
+      }
 }
